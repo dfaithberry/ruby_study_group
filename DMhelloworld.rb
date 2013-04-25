f9b95956661327fedbda092436f5440a6f0b122b
dwhelan <dwhelan@rallydev.com>
2013-04-25 09:22:24 -0400

    this is my first commit with github & my hello world project

---
+++ DMhelloworld.rb
@@ -0,0 +1,8 @@
+=begin
+Dina's homework for Ruby Brigade Raleigh
+This will display 'Hello World'
+=end
+puts 'Please enter your first name'
+STDOUT.flush
+name = gets.chomp
+prints name "says Hello World"
